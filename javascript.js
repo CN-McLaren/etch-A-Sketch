8c40e1f88acab6fb255119d5396cc93caeb0a6f6
ChonkyVacation <chris.mclaren1@hotmail.com>
2022-11-01 17:15:12 +1100

    Created start button

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateRows = 'repeat(16, 1fr)';
-board.style.gridTemplateColumns = 'repeat(16, 1fr)';
-
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div')
-    square.style.backgroundColor = 'blue';
-    board.insertAdjacentElement("beforeend", square);
+function createBoard() {
+    let board = document.querySelector(".board");
+    board.style.gridTemplateRows = 'repeat(16, 1fr)';
+    board.style.gridTemplateColumns = 'repeat(16, 1fr)';
+    
+    for (let i = 0; i < 256; i++) {
+        let square = document.createElement('div')
+        square.style.backgroundColor = 'blue';
+        board.insertAdjacentElement("beforeend", square);
+    }
 }
+
+let start = document.getElementById("start");
+start.addEventListener('click', event => {
+    createBoard();
+});
